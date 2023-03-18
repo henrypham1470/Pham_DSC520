4fdd7a5e96906920b6f261669f056a45d13adfaf
henrypham1470 <119464783+henrypham1470@users.noreply.github.com>
2023-03-18 00:49:37 -0400

    Add files via upload

---
+++ hello.R
@@ -0,0 +1,14 @@
+print("hello")
+id = c(1,2,3,4)
+sex = c("m","f","m","f")
+dat1 = data.frame(id, sex)
+dat1
+
+id = c(1,2,3,4,5)
+age = c(21,25,34,45,39)
+dat2 = data.frame(id, age)
+dat2
+
+dat1
+
+
